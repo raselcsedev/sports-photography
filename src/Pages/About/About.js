30a7f0c0435bf30ca88d6cb3ea5d2b74c7129f7f
raselcsedev <raselcse.dev@gmail.com>
2022-04-18 11:04:41 +0600

    designed header

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -6,15 +6,18 @@ const About = () => {
     return (
         <div className='about'>
             <div className='align-items-center justify-content-center w-100'>
-            <div>
-            <img className='image-container' src={rasel} alt="" />
+                <div>
+                    <img className='image-container' src={rasel} alt="" />
+                </div>
+                <div>
+                    <h2 className='w-100 '>Md. Rasel Aktar</h2>
+                    <p className='container'><small> "I am passionate about things related to HTML5, CSS3, JavaScript, React, React Router and React Firebase. Also, a hardworking professional focused on writing clean codes and learning new things. I am currently searching for the right opportunity where I can work in a friendly environment which will not only help me to progress into full-stack role but also add value to that organization"</small></p>
+                    <h3 className='text-success'>Get in Touch</h3>
+                    <p className='mb-0'>Email:<small className='text-primary mb-0'>raselcse.dev@gmail.com</small></p>
+                    <p className='mt-0'><small>Extension Pallabi, <br /> Dhaka-1216</small></p>
+                    {/* <button className='btn btn-success'>resume</button> */}
+                </div>
             </div>
-            <div>
-            <h2 className='w-100 '>Md. Rasel Aktar</h2>
-            <p><small> I am Rasel. I spend my whole day, practically every day, experimenting with HTML, CSS, JavaScript and React .</small></p>
-            <button className='btn btn-success'>resume</button>
-            </div>   
-        </div>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,11 @@
 import { signOut } from 'firebase/auth';
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../../firebase.init';
-
+import logo from '../../../images/logo/sports.jpg'
+import './Header.css'
 
 const Header = () => {
     const [user] = useAuthState(auth);
@@ -17,7 +18,7 @@ const Header = () => {
             <Navbar collapseOnSelect expand="lg" sticky='top' bg="success" variant="dark">
                 <Container>
                     <Navbar.Brand as={Link} to="/">
-                        {/* <img height={30} src={logo} alt="" /> */}
+                        <img className='logo' height={30} src={logo} alt="" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
@@ -25,13 +26,6 @@ const Header = () => {
                             <Nav.Link href="home">Home</Nav.Link>
                             <Nav.Link href="checkout">Checkout</Nav.Link>
                             <Nav.Link href="blogs">Blogs</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
                         </Nav>
                         <Nav>
                             <Nav.Link as={Link} to="about">About</Nav.Link>
